8800cd6f2a0d686397724e0053c5f796cff9b618
unknown <email_github_kamu@example.com>
2026-01-07 09:36:02 +0700

    Inisialisasi Project Aplikasi Matematika

---
+++ Aritmatika/src/aritmatika/Aritmatika.java
@@ -0,0 +1,20 @@
+  /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aritmatika;
+
+/**
+ *
+ * @author faradilla maudy
+ */
+public class Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Aritmatika/src/aritmatika/Operations.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aritmatika;
+
+/**
+ *
+ * @author faradilla maudy
+ */
+public class Operations {
+    public double multiple(double a, double b){
+      return a*b;
+    }
+}
